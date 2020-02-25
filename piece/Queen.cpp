2866e99498abd3eabed754087643cd7a0826fc48
david <david.marcinkovic@fer.hr>
2020-02-25 14:55:01 +0100

    Added available moves for queen

--- piece/Queen.cpp
+++ piece/Queen.cpp
@@ -12,5 +12,15 @@ Queen::Queen(int x, int y, PieceColor color) : Piece(x, y, color)
 
 void Queen::getAvailableMoves()
 {
+    moves.clear();
+    moves.emplace_back(std::make_pair(destRect.x, destRect.y));
 
+    bool direction[] = {true, true, true, true};
+    constexpr const int indices1[] = {0, 0, 1, -1};
+    constexpr const int indices2[] = {1, -1, 0, 0};
+    constexpr const int indices3[] = {1, 1, -1, -1};
+    constexpr const int indices4[] = {1, -1, 1, -1};
+
+    insertMoves(direction, indices1, indices2, 4);
+    insertMoves(direction, indices3, indices4, 4);
 }
