f6727a4128af0180fa5886deace781bff89c4b94
david <david.marcinkovic@fer.hr>
2020-03-03 22:24:18 +0100

    Refactored code

--- factory/Util.h
+++ factory/Util.h
@@ -26,8 +26,8 @@ struct ChessSquares
      */
     static constexpr std::array<const char *, 16> getWhitePieces()
     {
-        return std::array<const char *, 16>{"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
-                                            "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"};
+        return std::array<const char *, 16>{"a1", "b1", "c1", "d1", "f1", "g1", "h1",
+                                            "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "e1"};
     }
 
     /**
@@ -39,8 +39,8 @@ struct ChessSquares
      */
     static constexpr std::array<const char *, 16> getBlackPieces()
     {
-        return std::array<const char *, 16>{"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-                                            "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"};
+        return std::array<const char *, 16>{"a8", "b8", "c8", "d8", "f8", "g8", "h8",
+                                            "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "e8"};
     }
 
     /**
