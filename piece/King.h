f0571d927aaff94b21893a027ebd623f78badabc
david <david.marcinkovic@fer.hr>
2020-03-03 21:24:21 +0100

    Added documentation for addcastleMove

--- piece/King.h
+++ piece/King.h
@@ -16,14 +16,48 @@ class King : public Piece
 {
 private:
 
+    /**
+     * Method used to add castle move.
+     */
     void addCastleMove();
 
+    /**
+     * Method returns square according to given column and
+     * piece color.
+     *
+     * @param col Column of the square.
+     * @return Position of the given square.
+     */
     std::pair<int, int> getSquare(char col);
 
+    /**
+     * Method used to check if squares between rook and
+     * king are occupied or not.
+     *
+     * @param numberOfSquares Number of squares in between rook and king.
+     * @param col Column of the square.
+     * @return True if all squares between rook and king are not occupied.
+     */
     bool checkSquaresInBetween(int numberOfSquares, char col);
 
+    /**
+     * Method used to check if king and rook are at they starting positions.
+     *
+     * @param rookPosition Column of the rook. For big castle it is 'a' and
+     *                      for small castle is 'h'.
+     * @param newKingPosition Column of the new king position. For big castle it
+     *                      is 'c' and for small is 'g'.
+     */
     void checkKingAndRook(char rookPosition, char newKingPosition);
 
+    /**
+     * Method used to add castle.
+     *
+     * @param numberOfSquares Number of squares in between rook and king.
+     * @param col Starting column of the squares in between rook and king.
+     * @param rookPosition Rook position. 'h' for small castle and 'a' for big castle.
+     * @param newKingPosition New king position. For big castle it is 'c' and for small is 'g'.
+     */
     void addCastleMove(int numberOfSquares, char col, char rookPosition, char newKingPosition);
 
 public:
