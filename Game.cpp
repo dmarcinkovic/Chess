d92ee655a6a630191739d4782f87babee2c93876
david <david.marcinkovic@fer.hr>
2020-11-09 23:44:28 +0100

    Clean up switch statement

--- Game.cpp
+++ Game.cpp
@@ -8,72 +8,69 @@ PieceColor Game::turn = PieceColor::WHITE;
 SDL_Window *Game::window;
 
 Game::Game(const char *title, int w, int h)
-        : running{true}, checkObserver(std::make_unique<CheckObserver>())
+		: running{true}, checkObserver(std::make_unique<CheckObserver>())
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED_MASK,
-                              SDL_WINDOWPOS_CENTERED_MASK, w, h, false);
+	window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED_MASK,
+							  SDL_WINDOWPOS_CENTERED_MASK, w, h, false);
 
-    renderer = SDL_CreateRenderer(window, -1, false);
+	renderer = SDL_CreateRenderer(window, -1, false);
 
-    board = std::make_unique<Board>(w, h, "resources/board.jpg");
+	board = std::make_unique<Board>(w, h, "resources/board.jpg");
 
-    pieces = std::make_shared<Pieces>();
-    pieces->getAvailableMoves();
-    pieces->addObserver(checkObserver.get());
+	pieces = std::make_shared<Pieces>();
+	pieces->getAvailableMoves();
+	pieces->addObserver(checkObserver.get());
 }
 
 Game::~Game()
 {
-    SDL_DestroyRenderer(renderer);
+	SDL_DestroyRenderer(renderer);
 }
 
 void Game::handleEvents()
 {
-    while (SDL_PollEvent(&event) != 0 && event.type == SDL_MOUSEMOTION);
+	while (SDL_PollEvent(&event) != 0 && event.type == SDL_MOUSEMOTION);
 
-    switch (event.type)
-    {
-        case SDL_QUIT :
-            running = false;
-            break;
-        case SDL_MOUSEBUTTONDOWN :
-            if (event.button.button == SDL_BUTTON_LEFT)
-                eventManager->mousePressed(pieces->getPiece(
-                        event.button.x, event.button.y), event);
-            break;
-        case SDL_MOUSEBUTTONUP :
-            if (event.button.button == SDL_BUTTON_LEFT)
-                eventManager->mouseReleased(event, pieces.get());
-            break;
-        case SDL_MOUSEMOTION :
-            eventManager->mouseMoved(event);
-            break;
-        case SDL_KEYDOWN :
-            if (event.key.keysym.sym == SDLK_f)
-            {
-                board->flip();
-            }
-            break;
-        default:
-            break;
-    }
+	switch (event.type)
+	{
+		case SDL_QUIT :
+			running = false;
+			break;
+		case SDL_MOUSEBUTTONDOWN :
+			if (event.button.button == SDL_BUTTON_LEFT)
+				eventManager->mousePressed(pieces->getPiece(event.button.x, event.button.y), event);
+			break;
+		case SDL_MOUSEBUTTONUP :
+			if (event.button.button == SDL_BUTTON_LEFT)
+				eventManager->mouseReleased(event, pieces.get());
+			break;
+		case SDL_MOUSEMOTION :
+			eventManager->mouseMoved(event);
+			break;
+		case SDL_KEYDOWN :
+			if (event.key.keysym.sym == SDLK_f)
+				board->flip();
+			break;
+		default:
+			break;
+	}
 }
 
 bool Game::isRunning() const
 {
-    return running;
+	return running;
 }
 
 void Game::draw()
 {
-    SDL_RenderClear(renderer);
+	SDL_RenderClear(renderer);
 
-    board->draw();
-    pieces->draw();
+	board->draw();
+	pieces->draw();
 
-    SDL_RenderPresent(renderer);
+	SDL_RenderPresent(renderer);
 }
 
 SDL_Window *Game::getWindow()
