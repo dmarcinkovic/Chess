9f39ae6d91caafef4f061623972d33e390f53c88
david <david.marcinkovic@fer.hr>
2021-02-06 14:08:11 +0100

    Done with UndoManager

--- undo/UndoManager.cpp
+++ undo/UndoManager.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "UndoManager.h"
+
+UndoManager UndoManager::getInstance()
+{
+	static UndoManager instance;
+
+	return instance;
+}
+
+void UndoManager::undo()
+{
+	if (undoStack.empty())
+	{
+		return;
+	}
+
+	auto action = undoStack.top();
+	undoStack.pop();
+
+	redoStack.push(action);
+	action.executeUndo();
+}
+
+void UndoManager::redo()
+{
+	if (redoStack.empty())
+	{
+		return;
+	}
+
+	auto action = redoStack.top();
+	redoStack.pop();
+
+	undoStack.push(action);
+	action.executeDo();
+}
+
+void UndoManager::push(MoveAction moveAction)
+{
+	if (!redoStack.empty())
+	{
+		redoStack.pop();
+	}
+
+	undoStack.push(std::move(moveAction));
+}
+
--- undo/UndoManager.h
+++ undo/UndoManager.h
@@ -6,9 +6,26 @@
 #define CHESS_UNDOMANAGER_H
 
 
+#include <stack>
+#include "../actions/MoveAction.h"
+
 class UndoManager
 {
+private:
+	std::stack<MoveAction> undoStack;
+	std::stack<MoveAction> redoStack;
 
+public:
+	static UndoManager getInstance();
+
+	void undo();
+
+	void redo();
+
+	void push(MoveAction moveAction);
+
+private:
+	UndoManager() = default;
 };
 
 
