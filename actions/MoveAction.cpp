146b568f785aa673a1dbf615427c2d3805827e33
david <david.marcinkovic@fer.hr>
2021-02-06 13:33:43 +0100

    Created MoveAction class

---
+++ actions/MoveAction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 06. 02. 2021..
+//
+
+#include "MoveAction.h"
---
+++ actions/MoveAction.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 06. 02. 2021..
+//
+
+#ifndef CHESS_MOVEACTION_H
+#define CHESS_MOVEACTION_H
+
+
+class MoveAction
+{
+
+};
+
+
+#endif //CHESS_MOVEACTION_H
